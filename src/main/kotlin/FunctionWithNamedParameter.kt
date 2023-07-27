56beb7e380a070091d9c6d84cec50050dae09ce3
MasKoding <masdidi00@gmail.com>
2023-07-27 11:18:36 +0700

    Update belajar kotlin

--- src/main/kotlin/FunctionWithNamedParameter.kt
+++ src/main/kotlin/FunctionWithNamedParameter.kt
@@ -2,6 +2,7 @@ fun biodata(nama:String,umur:Int,nis:Int,alamat:String){
     println("Nama : $nama \n Umur:$umur \n nis: $nis \n alamat:$alamat")
 }
 fun main(){
+    biodata("nama",14,1234,"bogor")
     biodata(nis=1234,nama="Ahmad",umur=15,alamat="Bogor")
     biodata(alamat="Jakarta",nama="Syahdan",nis=32145,umur=14)
 }
