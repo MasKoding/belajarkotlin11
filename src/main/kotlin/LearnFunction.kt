3dbd57304e42dd8297aba7bb9408761eb1744e7b
MasKoding <masdidi00@gmail.com>
2023-07-27 11:03:07 +0700

    Update belajar kotlin

--- src/main/kotlin/LearnFunction.kt
+++ src/main/kotlin/LearnFunction.kt
@@ -12,10 +12,13 @@ fun sayGoodluck(firstName:String,lastName:String=""){
     println("Goodluck $firstName $lastName")
 }
 
+fun sayGoodBye(firstName:String,lastName:String=""){
+    println("GoodBye $firstName $lastName")
+}
 
 fun main(){
     sayHello("Steve")
     sayGoodluck("Steve","Job")
     sayGoodluck("Daud")
-
+    sayGoodBye("Ahmad")
 }
