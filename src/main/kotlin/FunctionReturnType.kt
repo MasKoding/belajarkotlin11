0edb04a098ef9afa9194ccc6f24664242434eab4
MasKoding <masdidi00@gmail.com>
2023-07-27 11:38:37 +0700

    Update belajar kotlin

--- src/main/kotlin/FunctionReturnType.kt
+++ src/main/kotlin/FunctionReturnType.kt
@@ -6,5 +6,17 @@ fun main(){
     var resultSum:Int = sum(4,5)
     println("Sum a+b: $resultSum")
 
+    /**
+     *
+     *          Buat program kalkulator sederhana dengan buat file kalkulator
+     *          Input Angka 1:
+     *          Input Angka 2:
+     *          Penjumlahan Angka1 + Angka2 =
+     *          Perkalian Angka1 + Angka2 =
+     *          Pembagian Angka1 + Angka2 =
+     *          Pengurangan Angka1 + Angka2 =
+     *          Pangkat 2=  Angka1 * Angka2 =
+     *          Akar2 = sqrt(Angka1)
+     */
 
 }
